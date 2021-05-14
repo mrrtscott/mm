0316453119644a1893596e290cb3ac0b41f401a0
Scott <scott.r.ja@gmail.com>
2021-05-13 21:29:34 -0500

    Google dependencies removed

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,8 +28,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.azure.spring:azure-spring-boot-starter")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("com.google.cloud:spring-cloud-gcp-starter")
-	implementation("com.google.cloud:spring-cloud-gcp-starter-storage")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
